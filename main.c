6841fba0f62d5a973e234d5c4385bcbb93437e2a
imad <imad.el-mansouri@epita.fr>
2020-10-05 21:48:59 +0200

    ajout du main vide

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description: sortie du projet 
+ *        Version:  1.0
+ *        Created:  05/10/2020 20:00:34
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main()
+{
+	return 0;
+}
