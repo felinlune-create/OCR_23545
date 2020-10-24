9216ebcf74730b76d6146092ddcaf6c975f59d49
omar.seba <omar.seba@epita.fr>
2020-10-24 20:21:41 +0200

    changement de nom du main

---
+++ segmentation/mainomar.c
@@ -0,0 +1,13 @@
+// Created by mrsb75 on 24/10/2020.
+#include <stdio.h>
+#include "segmentation.h"
+
+const char *input = "~/f_study/OCR/Omar/segmentation";
+
+
+int main()
+{
+
+    segmentation(input);
+    return 0;
+}
